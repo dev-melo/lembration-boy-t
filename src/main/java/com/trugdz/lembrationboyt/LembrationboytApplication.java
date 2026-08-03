e66818bbff9d80578ffb0d289e9660ebb87edf08
Dev Melo <omelodev@gmail.com>
2026-08-02 23:31:24 -0300

    Iniciando projeto spring

---
+++ src/main/java/com/trugdz/lembrationboyt/LembrationboytApplication.java
@@ -0,0 +1,13 @@
+package com.trugdz.lembrationboyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembrationboytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembrationboytApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trugdz/lembrationboyt/LembrationboytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trugdz.lembrationboyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembrationboytApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
